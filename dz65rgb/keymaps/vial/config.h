0f38aa58f60dfa42b9909876bfae5def6da4fedf
Lykrin <piotr@egrodzki.com>
2022-07-24 02:30:31 -0700

    NEWER

---
+++ dz65rgb/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xBE, 0x84, 0x41, 0x18, 0xC1, 0x48, 0xCD, 0x24}
+#define VIAL_UNLOCK_COMBO_ROWS {4, 4}
+#define VIAL_UNLOCK_COMBO_COLS {0, 5}
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
+#define NO_ACTION_ONESHOT
